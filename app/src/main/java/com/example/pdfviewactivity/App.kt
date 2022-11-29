f310b44fe515295cddbd66a851740663d2039cf8
shankar <harryshankar@outlook.com>
2022-11-29 18:26:32 +0545

    added root detections

--- app/src/main/java/com/example/pdfviewactivity/App.kt
+++ app/src/main/java/com/example/pdfviewactivity/App.kt
@@ -1,12 +1,14 @@
 package com.example.pdfviewactivity
 
 import androidx.multidex.MultiDexApplication
+import com.example.pdfviewactivity.root_dections.RootUtil
 
 class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         DeviceHelper.me().init(this)
         DeviceHelper.me().bindService()
+
     }
 
     override fun onTerminate() {
--- app/src/main/java/com/example/pdfviewactivity/MainActivity.kt
+++ app/src/main/java/com/example/pdfviewactivity/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.animation.TranslateAnimation
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pdfviewactivity.databinding.ActivityMainBinding
+import com.example.pdfviewactivity.root_dections.RootUtil
 import com.usdk.apiservice.aidl.printer.AlignMode
 import com.usdk.apiservice.aidl.printer.OnPrintListener
 
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity(), DeviceHelper.ServiceReadyListener {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         DeviceHelper.me().setServiceListener(this)
-
+        if (RootUtil.isDeviceRooted()) finish()
 
         testVal = arrayOf(
             "      Taste Town Pvt. Ltd\r\n       Kathmandu , Nepal\r\n      VAT No. : 610307863\r\n          CREDIT NOTE\r\n                \r\nCN. No.: CN18-TFC-79/80     \r\nDate   : 11/28/2022         \r\nMiti   : 12/08/2079         \r\nName   :                          \r\nAddress:                    \r\nPAN No.:                    \r\nRef Bill No  : TI1061-TFC-79/80   \r\nRemarks: @remarks           \r\n--------------------------------\r\nSn|     Particulars    | Amount|\r\n--------------------------------\r\n1  MIX LAPHING DRY         97.35\r\nQty: 1\r\nRate: 97.35\r\n2  8848 VODKA HALF       1592.92\r\nQty: 1\r\nRate: 1592.92\r\n       -------------------------\r\n          Gross Amount:  1690.27\r\n          Discount    :     0.00\r\n          S. Charge   :     0.00\r\n          taxable     :  1690.27\r\n          nontaxable  :     0.00\r\n          VAT 13%     :   219.73\r\n          Net Amount  :  1910.00\r\n       -------------------------\r\n       Tender      :     1910.00\r\n       Change      :        0.00\r\n--------------------------------\r\nRs. One thousand nine hundred   \r\nten only                        \r\n                                \r\nThank You For Visiting Us.      \r\nHope You Had A Good Time.       \r\nCounter : Laphing   ( 3:35PM  ) \r\nCashier : Admin     \r\n-\r\n",
@@ -57,8 +58,8 @@ class MainActivity : AppCompatActivity(), DeviceHelper.ServiceReadyListener {
                 override fun onFinish() {}
                 override fun onError(error: Int) {}
             })
+        } catch (_: RemoteException) {
         }
-        catch (_: RemoteException) {}
 
 //        }
 
